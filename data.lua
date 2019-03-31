2a94f010f74f25851d2ccf91a937f8eae0be647f
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-31 13:20:20 +0100

    added existing code written by darkfrei: https://mods.factorio.com/mod/TagToTeleport

---
+++ data.lua
@@ -0,0 +1,11 @@
+data:extend ({
+  {
+    type = "virtual-signal",
+    name = "teleport-tag",
+    icon = "__TagToTeleport__/graphics/icon.png",
+    icon_size = 32,
+    subgroup = "virtual-signal-color",
+    order = "d[colors]-z[9dot]-z[teleport-tag]"
+  }
+
+})
