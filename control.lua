2be9c84c6ef8e5803f9e2a3f79844001b571a29a
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-31 16:09:36 +0100

    added handling for removing and modifying tags

--- control.lua
+++ control.lua
@@ -18,14 +18,27 @@ script.on_event(defines.events.on_chart_tag_added, function(event)
 end)
 
 script.on_event(defines.events.on_chart_tag_modified, function(event)
-    -- TODO if it was a fixed teleport location
-        -- if its name got changed to invalid, delete tag and remove it from globals
-        -- if its name got changed to valid one but it's a duplicate, delete tag and remove it from globals
-        -- if its name got changed to valid one, change its position in globals
+    -- if was a fixed teleport
+    if event.old_icon and event.old_icon.name == "TagToTeleport_teleport-tag" then
+        local teleport_number = teleport_number_from_text(event.old_text)
+        destroy_fixed_teleport_location(teleport_number)
+    end
+
+    -- if it is now a fixed teleport
+    if event.tag.icon and event.tag.icon.name == "TagToTeleport_teleport-tag" then
+        local player = game.players[event.player_index]
+        create_fixed_teleport_location(player, event.tag)
+    end
+
 end)
 
 script.on_event(defines.events.on_chart_tag_removed, function(event)
-    -- TODO if it was a fixed teleport location, remove it from globals
+    if not event.tag.icon or event.tag.icon.name ~= "TagToTeleport_teleport-tag" then
+        return
+    end
+
+    local teleport_number = teleport_number_from_tag(event.tag)
+    destroy_fixed_teleport_location(teleport_number)
 end)
 
 
@@ -39,8 +52,7 @@ end
 
 
 function create_fixed_teleport_location(player, tag)
-    local first_char = string.sub(tag.text, 1, 1)
-    local teleport_number = tonumber(first_char)
+    local teleport_number = teleport_number_from_tag(tag)
 
     if teleport_number == nil then
         player.print("Invalid name! First character must be a digit!")
@@ -55,6 +67,12 @@ function create_fixed_teleport_location(player, tag)
         game.print("Teleport '" .. tag.text .. "' created")
         global.teleports[teleport_number] = tag
     end
+end
+
+function destroy_fixed_teleport_location(teleport_number)
+    local teleport_name = global.teleports[teleport_number].text
+    global.teleports[teleport_number] = nil
+    game.print("Teleport '" .. teleport_name .. "' removed")
 end
 
 function teleport_player_to_fixed_teleport_location(player, teleport_number)
@@ -78,6 +96,14 @@ function teleport_player_to_tag(player, tag)
     end
 end
 
+
+function teleport_number_from_tag(tag)
+    return teleport_number_from_text(tag.text)
+end
+
+function teleport_number_from_text(text)
+    return tonumber(string.sub(text, 1, 1))
+end
 
 function distance(position_1, position_2)
     return math.floor(((position_1.x - position_2.x) ^ 2 + (position_1.y - position_2.y) ^ 2) ^ 0.5)
