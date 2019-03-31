0de064dcf355f155f5f12ab8a4cf3b70d8580613
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-31 17:08:24 +0100

    updated readme, info.json and changelog and bumped version to 1.0.0

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 # prepare zip file
-mkdir TagToTeleport_0.0.2
-cp info.json TagToTeleport_0.0.2
-cp README.md TagToTeleport_0.0.2
-cp MIT_license.txt TagToTeleport_0.0.2
-cp thumbnail.png TagToTeleport_0.0.2
-cp thumbnail.pdn TagToTeleport_0.0.2
-cp *.lua TagToTeleport_0.0.2
-cp -R graphics TagToTeleport_0.0.2
-cp -R locale TagToTeleport_0.0.2
-cp changelog.txt TagToTeleport_0.0.2
-zip -r TagToTeleport_0.0.2{.zip,}
+mkdir TagToTeleport_1.0.0
+cp info.json TagToTeleport_1.0.0
+cp README.md TagToTeleport_1.0.0
+cp LICENSE TagToTeleport_1.0.0
+cp thumbnail.png TagToTeleport_1.0.0
+cp thumbnail.pdn TagToTeleport_1.0.0
+cp *.lua TagToTeleport_1.0.0
+cp -R graphics TagToTeleport_1.0.0
+cp -R locale TagToTeleport_1.0.0
+cp changelog.txt TagToTeleport_1.0.0
+zip -r TagToTeleport_1.0.0{.zip,}
 
 # move zip to factorio mods folder (overriding existing one if present)
-mv TagToTeleport_0.0.2.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
+mv TagToTeleport_1.0.0.zip "/cygdrive/c/Users/${USER}/AppData/Roaming/Factorio/mods"
 
 # cleanup
-rm -r TagToTeleport_0.0.2
+rm -r TagToTeleport_1.0.0
