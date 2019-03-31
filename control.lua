0555509ae7b24c56e40f5fcfff350ea394ffd206
Jaroslaw Pawlak <jarcionek@users.noreply.github.com>
2019-03-31 13:39:47 +0100

    removed an option to teleport another player by typing their name into tag name

--- control.lua
+++ control.lua
@@ -4,7 +4,7 @@ script.on_event(defines.events.on_chart_tag_added, function(event)
     local surface = tag.surface
     local position = tag.position
 
-    local player = game.players[text] or game.players[event.player_index]
+    local player = game.players[event.player_index]
 
     if tag.icon and tag.icon.name == 'teleport-tag' then
         local new_position = surface.find_non_colliding_position('player', position, 128, 2)
